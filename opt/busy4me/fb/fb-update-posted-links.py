c57034a8fc0b59c5c9b8f864b0b2b3a6f3ef7a24
Visaroy <57727924+visaroy@users.noreply.github.com>
2019-11-23 00:01:45 +0000

    Create fb-update-posted-links.py

---
+++ opt/busy4me/fb/fb-update-posted-links.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import pyautogui
+import mss
+import mss.tools
+
+pyautogui.screenshot('/opt/busy/screenshot.png')
+
+with mss.mss() as sct:
+    # The screen part to capture
+    monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+    # Grab the data
+    sct_img = sct.grab(monitor)
+
+    # Save to the picture file
+    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)

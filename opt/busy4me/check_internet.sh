c666c23e18d01874f985b257c2a7ed41125826f4
porczynski <dariusz@porczynski.net>
2020-04-08 20:13:26 +0000

    ➕ more functions...

--- opt/busy4me/check_internet.sh
+++ opt/busy4me/check_internet.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+_extip () {
+  # what is my external IP
+  # ip
+_EXTIP=$(curl -s "http://whatismyip.akamai.com/")
+echo $_EXTIP
+}
+
 nc -z 8.8.8.8 53  >/dev/null 2>&1
 online=$?
 if [ $online -eq 0 ]; then

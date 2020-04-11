ee39b15e236495220171cd1956302bfee2549c10
porczynski <dariusz@porczynski.net>
2020-04-11 10:10:51 +0000

    🐯 correct functions

---
+++ opt/busy4me/fb/mouse-show.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import pyautogui, sys, subprocess
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print positionStr,
+        print '\b' * (len(positionStr) + 2),
+        sys.stdout.flush(),
+        subprocess.Popen(["xli", "-delay", "1", "-fork", "/usr/share/pixmaps/spin/spin02.gif"])
+except KeyboardInterrupt:
+    print '\n'

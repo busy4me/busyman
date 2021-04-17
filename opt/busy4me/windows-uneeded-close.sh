a562ea8aba93a07733955b57478c15156b8fd1cf
porczynski <dariusz@porczynski.net>
2021-04-17 11:06:34 +0000

    ☀️ add window "Feedback" to close

--- opt/busy4me/windows-uneeded-close.sh
+++ opt/busy4me/windows-uneeded-close.sh
@@ -5,16 +5,21 @@ LOGFILE=/var/log/busy4me_cron.log
 source /opt/busy4me/fb/fb-config
 
 # close uneeded windows every 1s
-window2="open file"
+window02="open file"
+window03="Feedback"
 
-while true; do 
-	check1=$(xdotool search --onlyvisible --name "$window2")
-	
+while true; do
+# method with xdotool
+	check1=$(xdotool search --onlyvisible --name "$window02")
 	if (($check1 > 0))
 	then
-		export DISPLAY=:0 && xdotool search --onlyvisible --name $window2 windowactivate
+		export DISPLAY=:0 && xdotool search --onlyvisible --name $window02 windowactivate
 		export DISPLAY=:0 && xdotool key Escape
 	fi
+
+# method with wmctrl (no need no activating window)
+	wmctrl -c "$window03"
+
+# do this every 5s
 	sleep 5
 done
-

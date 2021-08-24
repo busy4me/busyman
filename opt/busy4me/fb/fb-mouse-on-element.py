c7111a0d274ed5175538a6e9f31cd95211528cb2
porczynski <dariusz@porczynski.net>
2021-08-24 19:44:57 +0000

    ⁉️ add options by getopt

--- opt/busy4me/fb/fb-mouse-on-element.py
+++ opt/busy4me/fb/fb-mouse-on-element.py
@@ -1,50 +1,70 @@
 #!/usr/bin/env python
 
-import pyautogui, time, sys, math, random, os
+import pyautogui, time, sys, math, random, os, getopt, numpy, imutils, cv2
 from time import sleep
-import numpy
-import imutils
-import cv2
-
 pyautogui.FAILSAFE = False # disables the fail-safe
 
 script_dir = os.path.dirname(__file__)
 rel_path = "img/"
 img_path = os.path.join(script_dir, rel_path)
 
-# list of pictures to find on the screen
-list = [img_path+'fb_logo_01.png', img_path+'fb_logo_02.png']
+def options(argv):
+  element = ''
+  action = ''
+  try:
+     opts, args = getopt.getopt(argv,"he:a:",["element=","action="])
+  except getopt.GetoptError:
+      print 'usage: fb-mouse-on-element.py -e <element> -a <action>'
+      sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+        print '\033[1;32m usage: fb-mouse-on-element.py -e <element> -a <action>\033[1;0m'
+        sys.exit()
+    elif opt in ("-e", "--element"):
+        element = arg
+    elif opt in ("-a", "--action"):
+        action = arg
+  print 'Element to find is:\033[1;32m', element, '\033[1;0m'
+  print 'Action to do is:\033[1;33m', action, '\033[1;0m'
+  return element, action
 
-# click on the first chat person
+options(sys.argv[1:])
+
+# list of pictures to find on the screen
+list = [img_path+'fb_logo_01.png', img_path+'fb_logo_02.png', img_path+'800x600-ch75%-button-English(US).png']
+
+element, action = options(sys.argv[1:])
+
 def find_element ():
-    print list[0]
-    element = pyautogui.locateOnScreen(list[0], confidence=0.9)
-    element
-    imag = list[0]
-    print(element)
-    if element != None:
+#  print 'Element to find is:\033[1;32m', element, '\033[1;0m'
+#  print 'Action to do is:\033[1;33m', action, '\033[1;0m'
+  print list[0]
+  element = pyautogui.locateOnScreen(list[0], confidence=0.9)
+  element
+  imag = list[0]
+  print(element)
+  if element != None:
         element_x, element_y = pyautogui.center(element)
-        sys.stdout.write("\033[1;32m") # green print
-        print "found %r ... element ... what todo?" % imag
+        print "\033[1;32m ...found: %r element \033[1;33m...what todo?\033[1;0m" % imag
         print element_x, element_y
         sys.stdout.write("\033[1;0m")
         pyautogui.moveTo(element_x, element_y, 0.1, pyautogui.easeOutQuad)
-        r = random.randint(15,16)
-        for i in range(0, 54):
-          x = element_x+r*math.cos(float(i)*beta)
-          y = element_y+r*math.sin(float(i)*beta)
-          ixy = 'i'+'x'+'y'
-          print "step %r" % ixy
-#		  print "     %r" % x
-#		  print "     %r" % y
+        r = random.randint(15,30)
+        for i in range(0, 16):
+          x = round(element_x+r*math.cos(float(i)*beta))
+          y = round(element_y+r*math.sin(float(i)*beta))
+          ixy = i+x+y
+          print "step i:%r" %i, "radius r:",r, "x:",x, "y:",y
+#		print "     %r" % x
+#		print "     %r" % y
           pyautogui.moveTo(x, y, 0.1)
-#        pyautogui.moveTo(1+(element_x*20), 1+(element_y*30), (element_x*0.1), pyautogui.easeOutQuad)
-#        pyautogui.moveTo(element_x-50, element_y+20, 0.3, pyautogui.easeOutQuad)
-#        pyautogui.moveTo(element_x+50, element_y, 0.3, pyautogui.easeOutQuad)
+#       pyautogui.moveTo(1+(element_x*20), 1+(element_y*30), (element_x*0.1), pyautogui.easeOutQuad)
+#       pyautogui.moveTo(element_x-50, element_y+20, 0.3, pyautogui.easeOutQuad)
+#       pyautogui.moveTo(element_x+50, element_y, 0.3, pyautogui.easeOutQuad)
 
-    else:
+  else:
         sys.stdout.write("\033[1;31m") # red print
-        print "element not found %r ... move random" % imag
+        print "element %r not found ... move random" % imag
         sys.stdout.write("\033[1;0m")
         pyautogui.moveTo(679, 14, 1, pyautogui.easeOutQuad)
         pyautogui.moveTo(579, 34, 0.3, pyautogui.easeOutQuad)

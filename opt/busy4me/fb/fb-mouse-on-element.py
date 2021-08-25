c6cb74dc8612d4803ce6202fa945557f3ddc03ad
porczynski <dariusz@porczynski.net>
2021-08-25 18:56:24 +0000

    :squirrel: ok

--- opt/busy4me/fb/fb-mouse-on-element.py
+++ opt/busy4me/fb/fb-mouse-on-element.py
@@ -9,45 +9,45 @@ rel_path = "img/"
 img_path = os.path.join(script_dir, rel_path)
 
 def options(argv):
-  element = ''
-  action = ''
+  img = '' # image to find
+  action = '' # action to do
   try:
-     opts, args = getopt.getopt(argv,"he:a:",["element=","action="])
+     opts, args = getopt.getopt(argv,"hi:a:",["img=","action="])
   except getopt.GetoptError:
-      print 'usage: fb-mouse-on-element.py -e <element> -a <action>'
+      print 'usage: fb-mouse-on-element.py -p <img> -a <action>'
       sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-        print '\033[1;32m usage: fb-mouse-on-element.py -e <element> -a <action>\033[1;0m'
+        print '\033[1;32m usage: fb-mouse-on-element.py -p <img> -a <action>\033[1;0m'
         sys.exit()
-    elif opt in ("-e", "--element"):
-        element = arg
+    elif opt in ("-i", "--img"):
+        img = arg
     elif opt in ("-a", "--action"):
         action = arg
-  print 'Element to find is:\033[1;32m', element, '\033[1;0m'
-  print 'Action to do is:\033[1;33m', action, '\033[1;0m'
-  return element, action
+  return img, action
 
-options(sys.argv[1:])
+# options(sys.argv[1:])
 
-# list of pictures to find on the screen
-list = [img_path+'fb_logo_01.png', img_path+'fb_logo_02.png', img_path+'800x600-ch75%-button-English(US).png']
+# list of images to find on the screen
+# list = [img_path+'fb_logo_01.png', img_path+'fb_logo_02.png', img_path+'800x600-ch75%-button-English(US).png']
 
-element, action = options(sys.argv[1:])
+# read values from options and set as globals
+img, action = options(sys.argv[1:])
 
 def find_element ():
-#  print 'Element to find is:\033[1;32m', element, '\033[1;0m'
-#  print 'Action to do is:\033[1;33m', action, '\033[1;0m'
-  print list[0]
-  element = pyautogui.locateOnScreen(list[0], confidence=0.9)
+  print 'find_element: Image to find:\033[1;32m', img, '\033[1;0m'
+  print 'find_element: Action to do:\033[1;33m', action, '\033[1;0m'
+#  print list[0]
+#  print img
+#  element = pyautogui.locateOnScreen(list[0], confidence=0.9)
+  element = pyautogui.locateOnScreen(img_path+img, confidence=0.9)
   element
-  imag = list[0]
+#  imag = list[0]
   print(element)
   if element != None:
         element_x, element_y = pyautogui.center(element)
-        print "\033[1;32m ...found: %r element \033[1;33m...what todo?\033[1;0m" % imag
-        print element_x, element_y
-        sys.stdout.write("\033[1;0m")
+        print "\033[1;32m ...found: %r element \033[1;33m...what todo?\033[1;0m" % img
+        print "pyautogui.center coordinates:", element_x, element_y
         pyautogui.moveTo(element_x, element_y, 0.1, pyautogui.easeOutQuad)
         r = random.randint(15,30)
         for i in range(0, 16):
@@ -64,16 +64,16 @@ def find_element ():
 
   else:
         sys.stdout.write("\033[1;31m") # red print
-        print "element %r not found ... move random" % imag
+        print "element %r not found ... move random" % img
         sys.stdout.write("\033[1;0m")
         pyautogui.moveTo(679, 14, 1, pyautogui.easeOutQuad)
         pyautogui.moveTo(579, 34, 0.3, pyautogui.easeOutQuad)
         pyautogui.moveTo(679, 14, 0.3, pyautogui.easeOutQuad)
-        pyautogui.click(679, 14) # click on the chat icon
+#        pyautogui.click(679, 14) # mouse click
         pyautogui.moveTo(425, 77, 1, pyautogui.easeOutQuad)
         pyautogui.moveTo(491, 167, 1, pyautogui.easeOutQuad)
         pyautogui.moveTo(458, 77, 1, pyautogui.easeOutQuad)
-        pyautogui.click(458, 77) # click on the first profile in chat list
+#        pyautogui.click(458, 77) # mouse click
         pyautogui.moveTo(679, 14, 0.3, pyautogui.easeOutQuad)
         sys.stdout.write("\033[1;32m") # green print
 
